49d2784cef6bb99ef2d0e40e6932e9237072badc
Connor MacKenzie <connor@layerci.com>
2021-07-14 16:10:12 -0400

    refactor app structure

---
+++ docker-dash-app/docker-dashboard-front-end/src/dashboard/Container.js
@@ -0,0 +1,56 @@
+import React, {useEffect, useState} from 'react';
+import {useParams} from "react-router";
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+import {Button} from "@material-ui/core";
+import {Terminal} from 'xterm';
+
+const client = new W3CWebSocket('ws://127.0.0.1:3001/container/4c5c87ff50f8/logs');
+
+
+function Container() {
+    const [container, setContainer] = useState(null);
+    const [logs, setLogs] = useState([]);
+    const { id } = useParams();
+    const term = new Terminal();
+
+    useEffect(() => {
+        client.onopen= () => {
+            console.log("WS opened");
+        };
+
+        fetch("/container/" + id)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setContainer(result)
+                }
+            )
+
+    }, [])
+
+    const streamLogs = () => {
+
+        term.open(document.getElementById('terminal'));
+        client.send("logs");
+        client.onmessage = (data) => {
+            term.writeln(data.data)
+        }
+
+
+    }
+
+    return(
+        <div>
+            <h1>{id}</h1>
+            <Button
+                onClick={streamLogs}
+                variant={'outlined'}>
+                Connect to logs
+            </Button>
+            <div id="terminal"/>
+            {logs}
+        </div>
+    );
+}
+
+export default Container;
--- docker-dash-app/docker-dashboard-server/index.js
+++ docker-dash-app/docker-dashboard-server/index.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const app = express();
+const expressWs = require('express-ws')(app);
 const port = 3001;
 
 const Docker = require('dockerode');
 const docker = new Docker({socketPath: '/var/run/docker.sock'});
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
+const utf8 = require('utf8');
 
 app.get('/containers', (req, res) => {
     docker.listContainers({all: true}, (err, containers) => {
@@ -29,6 +28,32 @@ app.get('/container/:id', (req, res) => {
             res.send(container);
         }
     })
+});
+
+app.ws('/container/:id/logs', (ws, req) => {
+    ws.on('message', (msg) => {
+        console.log(msg);
+        let logOpts = {
+            stdout: true,
+            stderr: true,
+            follow: true
+        };
+
+        docker.getContainer(req.params.id).logs(logOpts, (err, logs) => {
+            if (err) {
+                console.log(err);
+                next(err);
+            } else {
+                logs.on('data', chunk =>{
+                    let encodedLogs = Buffer.from(chunk, 'utf-8').toString();
+                    ws.send(encodedLogs);
+                }
+                )
+
+            }
+        })
+    })
+
 });
 
 app.listen(port, () => {
