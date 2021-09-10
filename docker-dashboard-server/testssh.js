f2dfeac1bf0ed23460a74c5aa1ab28edd04ba7bb
Peter8234 <peter@webapp.io>
2021-09-09 23:00:09 -0400

    test ssh

---
+++ docker-dashboard-server/testssh.js
@@ -0,0 +1,21 @@
+const { Client } = require('ssh2');
+const conn = new Client();
+conn.on('ready', () => {
+  console.log('Client :: ready');
+  conn.exec('uptime', (err, stream) => {
+    if (err) throw err;
+    stream.on('close', (code, signal) => {
+      console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+      conn.end();
+    }).on('data', (data) => {
+      console.log('STDOUT: ' + data);
+    }).stderr.on('data', (data) => {
+      console.log('STDERR: ' + data);
+    });
+  });
+}).connect({
+  host: 'ip',
+  port: 22,
+  username: 'root',
+  password: 'password'
+});
